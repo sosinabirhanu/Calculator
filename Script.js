633567f1def66ffa44e42477f9d57b2f0a9df715
sosina <sosinabirhanu@gmail.com>
2024-08-06 05:16:46 +0845

    initial commit

---
+++ Script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("calc");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = "Error";
+  }
+}
